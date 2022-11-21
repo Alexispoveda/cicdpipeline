9a283273e8cd5b1bb960c32c23ab6d449c18acc2
Jose <jose137sp@gmail.com>
2022-11-21 09:13:06 -0500

    test.sh setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Jose Saavedra Jedi Master
+          Jose Saavedra Jedi Knight
         </p>
         <a
           className="App-link"
