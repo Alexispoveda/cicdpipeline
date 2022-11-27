19e208f257f5e43f8e629a4324fb54a6eef01588
Jose <jose137sp@gmail.com>
2022-11-27 10:12:15 -0500

    network mode del nuevo runner config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    // Change title of the page
+    document.title = process.env.NODE_ENV;
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Jose Saavedra Jedi Knight {process.env.NODE_ENV} DEPLOY Cositas😮‍💨
+        Grupo: Saavedra, Fumero, Guerra, Jurado, Bilbao.
+        </p>
+        <p>
+          Environment: {process.env.NODE_ENV} 😮‍💨
         </p>
         <a
           className="App-link"
